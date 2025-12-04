a6c0bc2ca3547a4634fbbdbf1d0da493ba676c2b
Manish2702-I <manish7056540236@gmail.com>
2025-12-04 19:11:07 +0530

    changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,27 +1,40 @@
-import React from 'react'
-import Grocery from "../../assets/grocery.png"
-import Button from '../Button/Button'
-import Title from '../Title/Title'
+import React from "react";
+import Grocery from "../../assets/grocery.png";
+import Button from "../Button/Button";
+import Title from "../Title/Title";
 
 const Hero = () => {
   return (
     <>
-    <div className='mt-20 flex items-center'>
-      <div className=' flex-1'>
-        <span className='bg-zinc-400 rounded-full px-4 py-2 items-center text-red-400'>Export Best Quality...</span>
-        <h1 className='mt-5 text-6xl font-bold'>Tasty Organic</h1>
-        <h1 className='text-6xl font-bold mt-2'><span className='text-red-500'>Fruits</span> & <span className='text-red-500'>Veggies</span></h1>
-        <h1 className='mt-2 text-6xl font-bold'>In Your City</h1>
-        <p className='mt-5 text-teal-500'>Bred for a high content of benificial substances. our prouducts are all freshand healthy</p>
-        <div><Button content = "Shop Now" /></div>
+      <div className="md:mt-20 md:flex items-center mt-10">
+        <div className=" flex-1">
+          <span className="bg-zinc-400 rounded-full px-4 py-2 items-center text-red-400">
+            Export Best Quality...
+          </span>
+          <h1 className="mt-5 md:text-6xl text-4xl font-bold">Tasty Organic</h1>
+          <h1 className="md:text-6xl text-4xl font-bold mt-2">
+            <span className="text-red-500">Fruits</span> &{" "}
+            <span className="text-red-500">Veggies</span>
+          </h1>
+          <h1 className="mt-2 md:text-6xl text-4xl font-bold">In Your City</h1>
+          <p className="md:mt-5 text-teal-500 mt-3 text-2xl">
+            Bred for a high content of benificial substances. our prouducts are
+            all freshand healthy
+          </p>
+          <div>
+            <Button content="Shop Now" />
+          </div>
+        </div>
+        <div className="md:flex-1">
+          <img
+            className="object-contain items-center md:h-[62vh] mt-7 "
+            src={Grocery}
+          />
+        </div>
       </div>
-      <div className='flex-1 ml-7'>
-        <img className="object-contain items-center h-[62vh]" src={Grocery} />
-      </div>
-    </div>
-    <Title name = "Shop by Catogary" />
+      <Title name="Shop by Catogary" />
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Navbar/Navebar.jsx
+++ src/components/Navbar/Navebar.jsx
@@ -1,32 +1,66 @@
-import React from 'react'
+import React from "react";
 import { IoSearch } from "react-icons/io5";
 import { FaHeart } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
+import { IoMenu } from "react-icons/io5";
 
 const Navebar = () => {
   return (
-    <div className='flex justify-between items-center'>
-      <a href="#" className='text-3xl font-bold'>
-        Gr<span className='text-orange-700'>O</span>cry
+    <div className="flex justify-between items-center">
+      <a href="#" className="md:text-3xl text-2xl font-bold">
+        Gr<span className="text-orange-700">O</span>cry
       </a>
       <div>
-        <ul className='flex items-center gap-x-10 text-xl font-semibold'>
-            <li><a href="#" className='hover:text-red-500'>Home</a></li>
-            <li><a href="#" className='hover:text-red-500'>About Us</a></li>
-            <li><a href="#" className='hover:text-red-500'>Process</a></li>
-            <li><a href="#" className='hover:text-red-500'>Contact Us</a></li>
+        <ul className=" md:flex items-center gap-x-10 text-xl font-semibold hidden">
+          <li>
+            <a href="#" className="hover:text-red-500">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:text-red-500">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:text-red-500">
+              Process
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:text-red-500">
+              Contact Us
+            </a>
+          </li>
         </ul>
       </div>
-      <div className='flex items-center gap-x-3'>
-        <div className='px-2 py-1 border-black border-2 rounded-full'>
-            <input type="text" placeholder='Searching...' className='px-2 py-1 outline-none text-sm' />
-            <button className='bg-amber-300 rounded-full items-center px-2 py-2'><IoSearch /></button>
+      <div className="md:flex items-center gap-x-3 hidden">
+        <div className="px-2 py-1 border-black border-2 rounded-full">
+          <input
+            type="text"
+            placeholder="Searching..."
+            className="px-2 py-1 outline-none text-sm"
+          />
+          <button className="bg-amber-300 rounded-full items-center px-2 py-2">
+            <IoSearch />
+          </button>
         </div>
-        <button className='text-xl'><FaHeart /></button>
-        <button className='text-xl'><FaShoppingCart /></button>
+        <div className="flex gap-5">
+          <button className="text-xl">
+            <FaHeart />
+          </button>
+          <button className="text-xl">
+            <FaShoppingCart />
+          </button>
+        </div>
+      </div>
+      <div>
+        <button className="text-xl md:hidden">
+          <IoMenu />
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navebar
+export default Navebar;
