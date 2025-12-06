27959e7b9958f1114d6fb808f428c5c4f83513ad
Manish2702-I <manish7056540236@gmail.com>
2025-12-06 20:27:39 +0530

    changes

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from '../Title/Title'
 
 const Product = () => {
+
+  const productlist = ['All' , 'Fruits' , 'Vegetables' , 'Dairy' , 'SeaFood'];
+  const [isActivetab , setisActivetab] = useState('All')
+
   return (
     <section>
         <div>
             <Title name = "Our Products" />
+            <div className='flex gap-5 justify-center mt-20'>
+              {productlist.map((items , index) =>{
+                return (
+                  <div className={`${isActivetab === items ? 'bg-orange-700 text-white' : 'bg-zinc-400'} px-3 py-2 rounded-lg text-2xl font-semibold`}>
+                    {items}
+                  </div>
+                )
+              })}
+            </div>
         </div>
     </section>
   )
